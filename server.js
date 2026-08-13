26b2584a38fe3392b18b00b4948e94024e6473b2
tropadoratomcleon-source <tropadoratomcleon@gmail.com>
2026-08-13 08:36:32 -0300

    update

--- server.js
+++ server.js
@@ -1,22 +1,38 @@
 import { fastify } from 'fastify';
+import { randomUUID } from 'node:crypto';
 
 const server = fastify();
 
-server.get('/', (req, res)=>{
-return 'Home - Cadastro de usuarios';
+server.get('/', (req, res) => {
+    return 'Home - Cadastro de usuarios';
 });
 
-server.get('/users', (req, res)=>{
-return 'Listagem de usuarios';
+server.get('/users', (req, res) => {
+    return 'Listagem de usuarios';
 });
 
-server.post('/users', (req, res)=>{
-return 'Cadastro de usuarios';
+server.post('/users', (req, res) => {
+    return 'ainda nao implementado';
 });
 
-server.listen({port: 3000}, (err, address) => {
-if (err) {
-console.error(err);
-}
-console.log( `Servidor rodando em ${address} ` );
+server.put('/users/:id', (req, res) => {
+    const id = req.params.id
+    return `Atualizado o usuario com ID ${id}`;
+});
+
+server.delete('/users/:id', (req, res) => {
+    const id = req.params.id
+    return `Deletado o usuario com ID ${id}`;
+});
+
+server.get('/users/:userId/pedido/:pedidoId', (req, res) => {
+    const { userId, pedidoId } = req.params;
+    return `Usuario ${userId}, Pedido ${pedidoId}`;
+});
+
+server.listen({ port: 3000 }, (err, address) => {
+    if (err) {
+        console.error(err);
+    }
+    console.log(`Servidor rodando em ${address} `);
 });
